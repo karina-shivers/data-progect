ecb648b464b7b80a1415b1d4666e84e0f2cf9d76
karina-shivers <106991636+karina-shivers@users.noreply.github.com>
2022-07-30 16:49:37 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
-/*
-Treehouse Techdegree:
+/*Treehouse Techdegree:
 FSJS Project 2 - Data Pagination and Filtering
 */
 
@@ -20,10 +19,6 @@ This function will create and insert/append the elements needed to display a "pa
 const itemsPerPage = 9;
 
 
-/*
-Create the `addPagination` function
-This function will create and insert/append the elements needed for the pagination buttons
-*/
 
 function showPage( list, page ) {
    const startIndex = (page * itemsPerPage) - itemsPerPage;
@@ -31,5 +26,75 @@ function showPage( list, page ) {
    let studentList = document.querySelector('.student-list');
 
    studentList.innerHTML = '';
+/*
+Create the `addPagination` function
+This function will create and insert/append the elements needed for the pagination buttons
+*/
+function addPagination(list) {
+   let numOfPages = Math.ceil(list.length / 9);
+   let linkList = document.querySelector('.link-list');
+   linkList.innerHTML = '';
+   for (let i = 1; i <= numOfPages; i++) {
+     let button = `
+         <li>
+         <button type="button">${i}</button>
+         </li>
+       `;
+ 
+     linkList.insertAdjacentHTML('beforeend', button);
+     const buttonActive = document.querySelector('li button');
+     buttonActive.className = 'active';
+   }
+ 
+   linkList.addEventListener('click', (e) => {
+     const button = e.target;
+     const pageNumber = button.textContent;
+     const buttonActive = linkList.querySelector('.active');
+     if (e.target.tagName === 'BUTTON') {
+       buttonActive.className = '';
+       button.className = 'active';
+       showPage(list, pageNumber);
+     }
+   });
+ 
+ }
+ // appends the html elements needed for the search input
+ function addSearchBar() {
+   const header = document.querySelector('.header');
+   const inputForm = document.createElement('label');
+   inputForm.innerHTML = `
+     <label for="search" class="student-search">
+       <span>Search by name</span>
+       <input id="search" placeholder="Search by name...">
+       <button id="submit" type="button"><img src="img/icn-search.svg" alt="Search icon"></button>
+     </label>
+   `;
+ 
+   header.appendChild(inputForm);
+ }
 
 // Call functions
+showPage(data, 1);
+addPagination(data);
+addSearchBar();
+function searchBar(searchInput, list) {
+   let resultArray = [];
+   for (let i = 0; i < list.length; i++) {
+     const firstName = list[i].name.first.toLowerCase();
+     const lastName = list[i].name.last.toLowerCase();
+     if (searchInput.value.length !== 0 && firstName.includes(searchInput.value.toLowerCase()) || lastName.includes(searchInput.value.toLowerCase())) {
+       resultArray.push(list[i]);
+       showPage(resultArray, 1)
+       addPagination(resultArray);
+     } else if (resultArray.length === 0) {
+       document.querySelector('.student-list').innerHTML = 'No Results';
+       document.querySelector('.link-list').innerHTML = '';
+     }
+   }
+ }
+ 
+ const search = document.querySelector('#search');
+ // Event listener on keyup for search filtering
+ search.addEventListener('keyup', () => {
+   searchBar(search, data);
+ })};
